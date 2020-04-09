7af5204fd3f6b6e75ca021550ca3b6b48c576bfd
Nash <nashx90@gmail.com>
2020-04-10 02:31:36 +0900

    Add debug menu

--- app/javascript/frontend/components/App.jsx
+++ app/javascript/frontend/components/App.jsx
@@ -7,12 +7,29 @@ import SignupFormContainer from "./session/signup_form_container";
 import LoginFormContainer from "./session/login_form_container";
 import NewPostFormContainer from "./posts/new_post_form_container";
 
+const DebugMenu = props => {
+  return (
+    props.show && (
+      <div className="debug-menu">
+        <h4>DEBUG MENU:</h4>
+        <Link to="/">Root</Link>
+        <Link to="/signup">Sign Up</Link>
+        <Link to="/login">Log In</Link>
+        <Link to="/users/3">Profile Show</Link>
+        <Link to="/feed">Feed</Link>
+        <Link to="/posts/new">New Post</Link>
+        <Link to="/posts/1">Post Show</Link>
+      </div>
+    )
+  );
+};
 const App = () => (
   <div className="app-container">
+    <DebugMenu show={true} />
     <header>
-      <Link to="/">
-        <h1>Welcome to Picbait</h1>
-      </Link>
+      <h1>
+        <Link to="/">picbait</Link>
+      </h1>
       <SessionGreetingContainer />
     </header>
     <main>
