199348c69f1c9605ae4556014bfb70d1657a823b
Nash <nashx90@gmail.com>
2020-04-09 22:13:22 +0900

    Consolidate misc utils, add UUID image filenames

--- app/javascript/frontend/util/csrf_util.js
+++
@@ -1,6 +0,0 @@
-export const token = function (xhr) {
-  xhr.setRequestHeader(
-    "X-CSRF-Token",
-    $('meta[name="csrf-token"]').attr("content")
-  );
-};
---
+++ app/javascript/frontend/util/misc_util.js
@@ -0,0 +1,25 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const token = function (xhr) {
+  xhr.setRequestHeader(
+    "X-CSRF-Token",
+    $('meta[name="csrf-token"]').attr("content")
+  );
+};
+
+export const capitalize = (string, separator = " ") => {
+  const wordsArray = string.split(separator).map(word => {
+    const chArray = word.split("");
+    chArray[0] = chArray[0].toUpperCase();
+    return chArray.join("");
+  });
+  return wordsArray.join(separator);
+};
+
+export const getFileExtension = filename => filename.split(".").pop();
+
+export const makeFilename = filename => {
+  const extension = filename.split(".").pop();
+  const uuid = uuidv4();
+  return uuid.concat(".", extension);
+};
