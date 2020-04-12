3f0d201a8c09758cf906b90dcc1f2024e07c8a0d
Nash <nashx90@gmail.com>
2020-04-12 23:48:41 +0900

    Seed database with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do |i|
+  username = Faker::Internet.unique.username(specifier: 6)
+  email = Faker::Internet.unique.safe_email(name: username)
+  bio = Faker::Quote.matz[0..150]
+  password = username
+  user = Api::User.new(username: username, email: email, bio: bio, password: password, lowercase_username: username.downcase)
+  avatar = File.open("temp_scratch/seed_lores_paintings/#{i % 37 + 1}.jpg")
+  user.avatar.attach(io: avatar, filename: "#{i % 37 + 1}.jpg")
+  user.save!
+end
