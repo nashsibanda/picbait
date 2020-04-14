e2802fec3fe3d739c32a9f4d8b6c363d708898e3
Nash <nashx90@gmail.com>
2020-04-14 17:43:32 +0900

    Destroy dependents, validate username chars

--- app/models/api/user.rb
+++ app/models/api/user.rb
@@ -4,14 +4,14 @@ class Api::User < ApplicationRecord
   extend FriendlyId
   attr_reader :password
 
-  has_many :posts, class_name: 'Api::Post', foreign_key: 'api_user_id'
-  has_many :comments, class_name: 'Api::Comment', foreign_key: 'api_user_id'
+  has_many :posts, class_name: 'Api::Post', foreign_key: 'api_user_id', dependent: :destroy
+  has_many :comments, class_name: 'Api::Comment', foreign_key: 'api_user_id', dependent: :destroy
   has_one_attached :avatar
 
   after_initialize :ensure_session_token
   before_validation :downcase_email
   validates :username, :lowercase_username, :email, :password_digest, presence: true
-  validate :no_at_in_username
+  validate :no_illegal_characters
   validates_uniqueness_of :username, :email, on: :create, message: 'is already in use. Please use another one!'
   validates_length_of :password, within: 6..20, on: :create, message: 'must be between 6 and 20 characters'
   validates_length_of :bio, maximum: 200, message: 'must be less than 200 characters'
@@ -47,9 +47,14 @@ class Api::User < ApplicationRecord
 
   private
 
-  def no_at_in_username
+  def no_illegal_characters
     if username.include?('@')
       errors.add(:username, "can't have an @ symbol in it.")
+    end
+    errors.add(:username, "can't end with a period.") if username.last === '.'
+    test_arr = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a + ['_', '-', '.']
+    unless username.chars.all? { |ch| test_arr.include?(ch) }
+      errors.add(:username, 'can only have `.`, `-`, `_` or alphanumeric characters.')
     end
   end
 
