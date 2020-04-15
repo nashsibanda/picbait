063535fde9645c53d2154e331bbd4d206f7a9abd
Nash <nashx90@gmail.com>
2020-04-16 02:03:43 +0900

    Wait for bio before loading followers

--- app/javascript/frontend/components/followers/followers_index_item.jsx
+++ app/javascript/frontend/components/followers/followers_index_item.jsx
@@ -5,24 +5,26 @@ import { makeShortString } from "../../util/misc_util";
 export const FollowersIndexItem = props => {
   const { id, slug, avatarUrl, username, bio } = props.user;
   return (
-    <li className="followers-index-item">
-      <div className="avatar">
-        <Link to={`/users/${slug}`}>
-          <button
-            type="button"
-            className="container"
-            style={{
-              backgroundImage: `url(${avatarUrl})`,
-            }}
-          ></button>
-        </Link>
-      </div>
-      <div className="details">
-        <div className="username">
-          <Link to={`/users/${slug}`}>{username}</Link>
+    bio && (
+      <li className="followers-index-item">
+        <div className="avatar">
+          <Link to={`/users/${slug}`}>
+            <button
+              type="button"
+              className="container"
+              style={{
+                backgroundImage: `url(${avatarUrl})`,
+              }}
+            ></button>
+          </Link>
         </div>
-        <div className="mini-bio">{makeShortString(bio, 40)}</div>
-      </div>
-    </li>
+        <div className="details">
+          <div className="username">
+            <Link to={`/users/${slug}`}>{username}</Link>
+          </div>
+          <div className="mini-bio">{makeShortString(bio, 40)}</div>
+        </div>
+      </li>
+    )
   );
 };
